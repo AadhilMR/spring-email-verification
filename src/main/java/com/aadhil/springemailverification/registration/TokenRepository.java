218e7aab30994e979272abd53561ae5c10ab3af6
AadhilMR <aadhil2001ahamed@gmail.com>
2024-06-24 11:23:37 +0530

    Create a child of JpaRepository for Token entity

---
+++ src/main/java/com/aadhil/springemailverification/registration/TokenRepository.java
@@ -0,0 +1,6 @@
+package com.aadhil.springemailverification.registration;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+}
