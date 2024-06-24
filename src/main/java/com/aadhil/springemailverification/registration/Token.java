046b677e623c5eff1af96c13bf0d9026dbcd748f
AadhilMR <aadhil2001ahamed@gmail.com>
2024-06-24 11:13:06 +0530

    Create Token class for user verification

---
+++ src/main/java/com/aadhil/springemailverification/registration/Token.java
@@ -0,0 +1,38 @@
+package com.aadhil.springemailverification.registration;
+
+import java.time.LocalDateTime;
+
+import com.aadhil.springemailverification.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private LocalDateTime expirationTime;
+
+    private static final int EXPIRATION_TIME = 5;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Token(String token, User user) {
+        this.token = token;
+        this.user = user;
+        expirationTime = LocalDateTime.now().plusMinutes(EXPIRATION_TIME);
+    }
+
+    public Token(String token) {
+        this.token = token;
+        expirationTime = LocalDateTime.now().plusMinutes(EXPIRATION_TIME);
+    }
+}
