0d3b124f35708970e6b697f5b854a50263831c9a
AadhilMR <aadhil2001ahamed@gmail.com>
2024-06-24 20:48:07 +0530

    Updated security configurations

--- src/main/java/com/aadhil/springemailverification/security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/aadhil/springemailverification/security/UserRegistrationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.aadhil.springemailverification.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class UserRegistrationSecurityConfig {
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
