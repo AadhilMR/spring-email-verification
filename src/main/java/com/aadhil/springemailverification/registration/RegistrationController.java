1ea2d69636bf599c1d690ffc78abd9f185ed55dd
AadhilMR <aadhil2001ahamed@gmail.com>
2024-06-24 10:51:50 +0530

    Publish event on registration

--- src/main/java/com/aadhil/springemailverification/registration/RegistrationController.java
+++ src/main/java/com/aadhil/springemailverification/registration/RegistrationController.java
@@ -1,11 +1,14 @@
 package com.aadhil.springemailverification.registration;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aadhil.springemailverification.event.RegistrationCompleteEvent;
 import com.aadhil.springemailverification.user.User;
 import com.aadhil.springemailverification.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -13,11 +16,18 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/register")
 public class RegistrationController {
     private final UserService userService;
+    private final ApplicationEventPublisher publisher;
 
     @PostMapping
-    public String register(RegistrationRequest registrationRequest) {
+    public String register(RegistrationRequest registrationRequest, final HttpServletRequest request) {
         User user = userService.registerUser(registrationRequest);
 
+        publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
+
         return "Success! Please, check your email to verify your account.";
+    }
+
+    private String applicationUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
     }
 }
