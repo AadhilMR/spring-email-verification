082e9c980f02758f81f218663acc8c58a89b1104
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-01 19:51:59 +0530

    Create & save verification token

--- src/main/java/com/aadhil/springemailverification/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/aadhil/springemailverification/listener/RegistrationCompleteEventListener.java
@@ -1,13 +1,37 @@
 package com.aadhil.springemailverification.listener;
 
+import java.util.UUID;
+
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
 import com.aadhil.springemailverification.event.RegistrationCompleteEvent;
+import com.aadhil.springemailverification.user.User;
+import com.aadhil.springemailverification.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
+
+    private final UserService userService;
 
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event) {
+        User user = event.getUser();
 
+        // Create new token
+        String verificationToken = UUID.randomUUID().toString();
+
+        // Save the token
+        userService.saveUserVerificationToken(user, verificationToken);
+
+        // Create a new URL for user verification
+        String url = event.getApplicationUrl() + "/register/verify?token=" + verificationToken;
+
+        // Send the email
+        log.info("Click the link to verify your registration : {}", url);
     }
 }
