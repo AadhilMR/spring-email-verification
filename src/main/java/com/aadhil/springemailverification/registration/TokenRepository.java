65b8653932e5d072db3c75a2f26a5868940ffc1b
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-14 17:49:18 +0530

    Update repository

--- src/main/java/com/aadhil/springemailverification/registration/TokenRepository.java
+++ src/main/java/com/aadhil/springemailverification/registration/TokenRepository.java
@@ -3,4 +3,5 @@ package com.aadhil.springemailverification.registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
+    Token findByToken(String token);
 }
