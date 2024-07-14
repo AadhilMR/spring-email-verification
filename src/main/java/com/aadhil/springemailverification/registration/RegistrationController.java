764883ba051052d98379b2a3edc6a57635a3b1bb
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-14 08:00:42 +0530

    Added the missing annotation

--- src/main/java/com/aadhil/springemailverification/registration/RegistrationController.java
+++ src/main/java/com/aadhil/springemailverification/registration/RegistrationController.java
@@ -2,6 +2,7 @@ package com.aadhil.springemailverification.registration;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class RegistrationController {
     private final ApplicationEventPublisher publisher;
 
     @PostMapping
-    public String register(RegistrationRequest registrationRequest, final HttpServletRequest request) {
+    public String register(@RequestBody RegistrationRequest registrationRequest, final HttpServletRequest request) {
         User user = userService.registerUser(registrationRequest);
 
         publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
