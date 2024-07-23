fd4333740a50844b0bc7617300de55aacac22a4b
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-23 23:27:51 +0530

    Add new end-point for request new verification

--- src/main/java/com/aadhil/springemailverification/registration/RegistrationController.java
+++ src/main/java/com/aadhil/springemailverification/registration/RegistrationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.aadhil.springemailverification.email.EmailSender;
 import com.aadhil.springemailverification.event.RegistrationCompleteEvent;
 import com.aadhil.springemailverification.user.User;
 import com.aadhil.springemailverification.user.UserService;
@@ -21,6 +22,7 @@ public class RegistrationController {
     private final UserService userService;
     private final ApplicationEventPublisher publisher;
     private final TokenRepository tokenRepository;
+    private final EmailSender emailSender;
 
     @PostMapping
     public String register(@RequestBody RegistrationRequest registrationRequest, final HttpServletRequest request) {
@@ -34,6 +36,17 @@ public class RegistrationController {
     @GetMapping("/verify")
     public String verifyEmail(@RequestParam("token") String token, final HttpServletRequest request) {
         return userService.validateToken(token, applicationUrl(request));
+    }
+
+    @GetMapping("/resend-token")
+    public String requestNewToken(@RequestParam("token") String oldToken, final HttpServletRequest request) {
+        Token newToken = userService.generateNewToken(oldToken);
+        User theUser = newToken.getUser();
+        String url = applicationUrl(request) + "/register/verify?token=" + newToken.getToken();
+
+        emailSender.send(theUser, url);
+
+        return "A new verification link has been sent to your email, please check to verify your account";
     }
 
     private String applicationUrl(HttpServletRequest request) {
