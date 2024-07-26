1d275e0f361439d756423eb15389aebb46ea3072
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-26 09:06:03 +0530

    Add config class for filter frontend requests

---
+++ src/main/java/com/aadhil/springemailverification/security/WebConfig.java
@@ -0,0 +1,26 @@
+package com.aadhil.springemailverification.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new MyWebMvcConfigurer();
+    }
+
+    private static class MyWebMvcConfigurer implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+    }
+}
